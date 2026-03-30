723227e2feeb62b5cbff057225f680fb057b6999
Alexander090608 <alexambrocio538@gmail.com>
2026-03-30 16:17:24 +0000

    added functionality to the boxes such that food is displayed once clicked

--- decision maker - 3-26-2026/script.js
+++ decision maker - 3-26-2026/script.js
@@ -1,8 +1,40 @@
-async function getInfo(file){
+async function getInfo(file) {
     let res = await fetch(file);
-    let data = await res.json();
+    data = await res.json();
+
+    //meal name, meal image, meal instructions
     let mealName = data.meals[0].strMeal; //data.(first element of data array).(meal name)
+    let mealImg = data.meals[0].strMealThumb; //data.(first element of data array).(meal image)
+    let mealInstructions = data.meals[0].strInstructions; //data.(first element of data array).(meal instructions)
     console.log(data.meals[0].strMeal);
+    console.log(data.meals[0].strMealThumb);
+    console.log(data.meals[0].strInstructions);
+
+    //result container
+    let grid = document.querySelector(".result-container");
+    if (grid) {
+        createMealDisplay(mealName, mealImg, mealInstructions);
+    }
+
+    function createMealDisplay(mealName, mealImg, mealInstructions) {
+        grid.innerHTML = `
+            <h2>${mealName}</h2>
+            <img style="width: 200px; height: 200px;" src="${mealImg}" alt="Meal Image">
+            <p>${mealInstructions}</p>
+        `
+    }
+
 }
 
-getInfo("https://www.themealdb.com/api/json/v1/1/random.php");
+function boxClick() {
+    for (let i = 1; i < 10; i++) {
+        let box = document.querySelector(`.box${i}`);
+        box.addEventListener("click", function () {
+            getInfo("https://www.themealdb.com/api/json/v1/1/random.php");
+            let grid = document.querySelector(".result-container");
+            grid.style.display = "grid";
+        });
+    }
+};
+
+boxClick();
